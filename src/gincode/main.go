149565592e00ace7b0e88db1fb0197fe92a528f9
jameinfeng <jameinfeng@tencent.com>
2021-12-08 18:53:54 +0800

    gin

--- src/gincode/main.go
+++ src/gincode/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/gin-gonic/gin"
 )
@@ -20,7 +21,8 @@ func Cors() gin.HandlerFunc {
 func main() {
 	router := gin.Default()
 	router.Use(Cors())
-
+	router.MaxMultipartMemory = 8 << 20 // 8 MiB
+	router.Static("/", "./public")
 	router.POST("/post", func(c *gin.Context) {
 		// c.Header("Access-Control-Allow-Origin", "*")
 		id := c.DefaultQuery("id", "11")
@@ -31,5 +33,36 @@ func main() {
 			"name": "jameinfeng",
 		})
 	})
+
+	router.POST("/upload", func(c *gin.Context) {
+		/** 单文件上传 */
+		// file, _ := c.FormFile("file")
+		// log.Println(file)
+		// c.SaveUploadedFile(file, "./public/"+file.Filename)
+
+		/** 多文件上传 */
+		form, _ := c.MultipartForm()
+		files := form.File["file"]
+
+		for _, file := range files {
+			log.Println(file.Filename)
+
+			// 上传文件至指定目录
+			c.SaveUploadedFile(file, "./public/"+file.Filename)
+		}
+
+		c.String(200, "上传成功")
+	})
+
+	authorized := router.Group("/cgi")
+	{
+		authorized.POST("/login", func(c *gin.Context) { c.String(200, "1") })
+		authorized.POST("/submit", func(c *gin.Context) { c.String(200, "1") })
+		authorized.POST("/read", func(c *gin.Context) { c.String(200, "1") })
+
+		// 嵌套路由组
+		testing := authorized.Group("testing")
+		testing.POST("/analytics", func(c *gin.Context) { c.String(200, "1") })
+	}
 	router.Run(":8080")
 }
