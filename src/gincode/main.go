3787e03fce6d712bc51d8d03422566f0e047665f
jameinfeng <jameinfeng@tencent.com>
2021-12-16 10:14:18 +0800

    设置路由组

--- src/gincode/main.go
+++ src/gincode/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"log"
 	"os"
@@ -35,64 +34,52 @@ func main() {
 
 	router.Use(Cors())
 	router.MaxMultipartMemory = 8 << 20 // 8 MiB
-	router.Static("/", "./public")
-	router.POST("/post", func(c *gin.Context) {
-		// c.Header("Access-Control-Allow-Origin", "*")
-		id := c.DefaultQuery("id", "11")
-		page := c.DefaultQuery("page", "0")
-
-		fmt.Printf("id: %s; page: %s", id, page)
-		c.JSON(200, gin.H{
-			"name": "jameinfeng",
-		})
-	})
-
-	router.POST("/upload", func(c *gin.Context) {
-		/** 单文件上传 */
-		// file, _ := c.FormFile("file")
-		// log.Println(file)
-		// c.SaveUploadedFile(file, "./public/"+file.Filename)
-
-		/** 多文件上传 */
-		form, _ := c.MultipartForm()
-		files := form.File["file"]
-
-		for _, file := range files {
-			log.Println(file.Filename)
-
-			// 上传文件至指定目录
-			c.SaveUploadedFile(file, "./public/"+file.Filename)
-		}
-
-		c.JSON(200, gin.H{
-			"message": "上传成功",
-		})
-	})
-
+	// router.Static("/", "./public")
 	authorized := router.Group("/cgi")
 	{
 		authorized.POST("/login", func(c *gin.Context) { c.String(200, "1") })
 		authorized.POST("/submit", func(c *gin.Context) { c.String(200, "1") })
 		authorized.POST("/read", func(c *gin.Context) { c.String(200, "1") })
+		authorized.POST("/upload", func(c *gin.Context) {
+			/** 单文件上传 */
+			// file, _ := c.FormFile("file")
+			// log.Println(file)
+			// c.SaveUploadedFile(file, "./public/"+file.Filename)
+
+			/** 多文件上传 */
+			form, _ := c.MultipartForm()
+			files := form.File["file"]
+
+			for _, file := range files {
+				log.Println(file.Filename)
+
+				// 上传文件至指定目录
+				c.SaveUploadedFile(file, "./public/"+file.Filename)
+			}
+
+			c.JSON(200, gin.H{
+				"message": "上传成功",
+			})
+		})
+		authorized.POST("/getUserInfo", func(c *gin.Context) {
+			var person Person
+			if (c.ShouldBindJSON(&person)) == nil {
+				log.Println(person.User)
+				log.Println(person.PassWord)
+				c.JSON(200, gin.H{
+					"name": "jamefeine",
+				})
+				return
+			}
+			c.JSON(200, gin.H{
+				"message": "密码错误",
+			})
+		})
 
 		// 嵌套路由组
 		testing := authorized.Group("testing")
 		testing.POST("/analytics", func(c *gin.Context) { c.String(200, "1") })
 	}
 
-	router.POST("/getUserInfo", func(c *gin.Context) {
-		var person Person
-		if (c.ShouldBindJSON(&person)) == nil {
-			log.Println(person.User)
-			log.Println(person.PassWord)
-			c.JSON(200, gin.H{
-				"name": "jamefeine",
-			})
-			return
-		}
-		c.JSON(200, gin.H{
-			"message": "密码错误",
-		})
-	})
-	router.Run(":8080")
+	router.Run(":3333")
 }
